6f42fbe84ec5733c1e4bb76d33f39fd933e82ef0
ChadM95 <ChadM95@users.noreply.github.com>
2024-05-18 00:33:01 +0100

    add image carousel to homepage

--- SurfShop_MVC/SurfShop_MVC/Models/Rental.cs
+++ SurfShop_MVC/SurfShop_MVC/Models/Rental.cs
@@ -11,18 +11,15 @@ namespace SurfShop_MVC.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Rental
     {
         public int Rental_ID { get; set; }
         public string Customer_Name { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:d}")]
         public Nullable<System.DateTime> Rental_Date { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:d}")]
         public Nullable<System.DateTime> Return_Date { get; set; }
         public Nullable<decimal> Price { get; set; }
+        public Nullable<int> NoOfWetsuits { get; set; }
+        public Nullable<int> NoOfBoards { get; set; }
     }
 }
